146872262dae0fadfa3fa5b55df286adf82fbac2
Patricia Magalhães <paath.magalhaes2@gmail.com>
2021-11-14 22:14:45 -0300

    primeiro exemplo com v-for

---
+++ V-for/script.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        list: [
+            { id: 1, name: 'Patricia' },
+            { id: 1, name: 'Pedro' },
+            { id: 1, name: 'Paulo' },
+            { id: 1, name: 'Pérola' },
+        ]
+    }
+})
